89d181c9bd53eeb5377c468877f29bce6a13e0e2
CharlieApell <charlie.apell@gmail.com>
2023-06-29 18:27:07 +0000

    deployment debugging

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -157,7 +157,7 @@ if 'DEV' in os.environ:
     }
 else:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL").encode())
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 
 

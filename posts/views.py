aeba3f7bd16f663cc9ce3605bcd110eecf4d1c43
CharlieApell <charlie.apell@gmail.com>
2023-06-29 21:11:36 +0000

    Correcting my mistakes and modifying views.py for both posts and profiles

--- posts/views.py
+++ posts/views.py
@@ -30,6 +30,8 @@ class PostList(generics.ListCreateAPIView):
     search_fields = [
         'owner__username',
         'title',
+        'content',
+        'category',
     ]
     ordering_fields = [
         'likes_count',

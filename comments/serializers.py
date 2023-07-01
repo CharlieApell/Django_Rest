cf3576faa038b697e4844456948edb455c1815ff
CharlieApell <charlie.apell@gmail.com>
2023-07-01 20:52:26 +0000

    troubleshooting why you suddenly can't comment

--- comments/serializers.py
+++ comments/serializers.py
@@ -14,7 +14,6 @@ class CommentSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     created_at = serializers.SerializerMethodField()
     updated_at = serializers.SerializerMethodField()
-    rating = serializers.CharField(max_length=255, source='get_rating_display')
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -30,7 +29,7 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'post', 'created_at', 'updated_at', 'content', 'rating',
+            'post', 'created_at', 'updated_at', 'content',
         ]
 
 
--- comments/views.py
+++ comments/views.py
@@ -17,8 +17,7 @@ class CommentList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(
-            owner=self.request.user,
-            rating=self.request.data.get('rating'))
+            owner=self.request.user)
 
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):

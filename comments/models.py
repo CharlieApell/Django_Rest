079368400895f7d4d7e9ecd625b403c2982f346d
CharlieApell <charlie.apell@gmail.com>
2023-07-02 20:30:29 +0000

    final touches before deployment, rating is a function i while implement in the future

--- comments/models.py
+++ comments/models.py
@@ -4,24 +4,12 @@ from posts.models import Post
 
 
 class Comment(models.Model):
-    
-    rating_choices = [
-        (0, ''),
-        (1, 'Bad'),
-        (2, 'Not good'),
-        (3, 'Neutral'),
-        (4, 'Good'),
-        (5, 'Really good'),
-    ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    rating = models.CharField(
-        max_length=255,
-        choices=rating_choices,
-        default='0')
 
     class Meta:
         ordering = ['-created_at']
